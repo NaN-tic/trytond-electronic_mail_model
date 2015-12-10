1d755c14162a08990f8a5891f85049f09ec24c5b
Sergi Almacellas Abellana <sergi@koolpi.com>
2015-12-10 23:00:16 +0100

    Migrate to version 3.8

--- tests/test_electronic_mail_model.py
+++ tests/test_electronic_mail_model.py
@@ -2,29 +2,15 @@
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends  # , doctest_dropdb TODO: Remove if no sceneario needed.
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('electronic_mail_model')
-
-    def test0005views(self):
-        'Test views'
-        test_view('electronic_mail_model')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'electronic_mail_imap'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
-    # TODO: remove if no scenario needed.
-    #suite.addTests(doctest.DocFileSuite('scenario_electronic_mail_model.rst',
-    #        setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='utf-8',
-    #        optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
